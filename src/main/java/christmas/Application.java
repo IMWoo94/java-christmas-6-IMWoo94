fe2f05b57eaa881e56c5edf528bda10a741de444
IMWoo94 <leemeawoo94@gmail.com>
2023-11-11 13:06:19 +0900

    feat(OutputView): 식당 예약 안내 멘트 출력 기능 구현
    식당 예약 안내 멘트
    "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다."
    "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)"
    출력
    
    Close# 식당 예약 안내 멘트 출력 기능

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,12 @@
 package christmas;
 
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 식당 예약 안내 멘트 출력 기능
+        OutputView.printOpening();
+        OutputView.printRequestReservationDate();
     }
 }
