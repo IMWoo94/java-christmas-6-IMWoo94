0d035845cac98d05216a4ff0a55ffde0894f36a2
IMWoo94 <leemeawoo94@gmail.com>
2023-11-12 15:06:03 +0900

    feat(getVariousMenu, validateDuplicateMenu): 주문 저장 시 중복 메뉴 없는 메뉴 입력 시 예외
    주문 내역 저장 시 중복 메뉴와 없는 메뉴 등록 시 예외 처리
    
    Close# 주문 입력 기능

--- src/main/java/christmas/domain/Orders.java
+++ src/main/java/christmas/domain/Orders.java
@@ -1,32 +1,58 @@
 package christmas.domain;
 
+import christmas.constants.ErrorMessage;
 import christmas.constants.VariousMenu;
-import java.util.Arrays;
+import christmas.exception.InvalidDataException;
 import java.util.EnumMap;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Orders {
     private final EnumMap<VariousMenu, Integer> orderMenu;
+    private final String orders;
 
     public Orders(String readOrder) {
-        orderMenuInit(readOrder);
-        orderMenu = Arrays.stream(readOrder.split(", "))
-                .map(order -> order.split("-"))
-                .collect(Collectors.toMap(
-                        arr -> VariousMenu.getResultByMatchedMenuName(arr[0]),
-                        arr -> Integer.parseInt(arr[1]),
-                        (existing, replacement) -> existing,
-                        () -> new EnumMap<>(VariousMenu.class)
-                ));
-    }
-
-    private void orderMenuInit(String readOrder) {
-        List<String> orders = Arrays.stream(readOrder.split(","))
-                .toList();
-
+        // 해산물파스타-2,레드와인-1,초코케이크-10 의 형식의 데이터
+        orders = readOrder;
+        orderMenu = new EnumMap<>(VariousMenu.class);
+        ordersInit();
 
     }
 
+    private void ordersInit() {
+        String orderRegex = "([^,]+)-(\\d+)";
+        Pattern pattern = Pattern.compile(orderRegex);
+        Matcher matcher = pattern.matcher(orders);
 
+        // 정규식에 일치하는 부분을 찾아 Map에 넣기
+        while (matcher.find()) {
+            String foodName = matcher.group(1);
+            int quantity = Integer.parseInt(matcher.group(2));
+
+            VariousMenu menuName = getVariousMenu(foodName);
+            validateDuplicateMenu(menuName);
+
+            orderMenu.put(menuName, quantity);
+        }
+    }
+
+    private VariousMenu getVariousMenu(String foodName) {
+        // 없는 메뉴 제공 시 예외 발생
+        return VariousMenu.getResultByMatchedMenuName(foodName);
+    }
+
+    private void validateDuplicateMenu(VariousMenu menuName) {
+        if (orderMenu.containsKey(menuName)) {
+            // 중복 입력에 대한 예외 발생 처리
+            throw new InvalidDataException(ErrorMessage.INVALID_DATA.getFormatMessage("주문"));
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Orders{" +
+                "orderMenu=" + orderMenu +
+                ", orders='" + orders + '\'' +
+                '}';
+    }
 }
