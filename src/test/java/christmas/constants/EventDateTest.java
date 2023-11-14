329295b64779783b3299adbadd7cdfde937b92ec
IMWoo94 <leemeawoo94@gmail.com>
2023-11-14 15:32:57 +0900

    test(상수 적용): 동일 매직넘버에 대한 값 상수 값 활용
    2023, 12 에 대한 값을 상수로 활용

--- src/test/java/christmas/constants/EventDateTest.java
+++ src/test/java/christmas/constants/EventDateTest.java
@@ -1,5 +1,7 @@
 package christmas.constants;
 
+import static christmas.constants.GlobalConstant.MONTH;
+import static christmas.constants.GlobalConstant.YEAR;
 import static christmas.constants.biz.EventDate.CHRISTMAST;
 import static christmas.constants.biz.EventDate.END_DATE;
 import static christmas.constants.biz.EventDate.START_DATE;
@@ -18,58 +20,58 @@ class EventDateTest {
         LocalDate startDate = START_DATE.getDate();
         LocalDate endDate = END_DATE.getDate();
 
-        assertThat(startDate).isEqualTo(LocalDate.of(2023, 12, 1));
-        assertThat(endDate).isEqualTo(LocalDate.of(2023, 12, 31));
+        assertThat(startDate).isEqualTo(LocalDate.of(YEAR, MONTH, 1));
+        assertThat(endDate).isEqualTo(LocalDate.of(YEAR, MONTH, 31));
     }
 
     @Test
     @DisplayName("[정상] 이벤트 기간 빼기")
     void eventDateMinusDays() {
         LocalDate date = START_DATE.getMinusDays(10);
-        assertThat(date).isEqualTo(LocalDate.of(2023, 12, 1).minusDays(10));
-        assertThat(date).isEqualTo(LocalDate.of(2023, 11, 21));
+        assertThat(date).isEqualTo(LocalDate.of(YEAR, MONTH, 1).minusDays(10));
+        assertThat(date).isEqualTo(LocalDate.of(YEAR, MONTH, 21));
     }
 
     @Test
     @DisplayName("[정상] 이벤트 기간 더하기")
     void eventDatePlusDays() {
         LocalDate date = START_DATE.getPlusDays(10);
-        assertThat(date).isEqualTo(LocalDate.of(2023, 12, 1).plusDays(10));
-        assertThat(date).isEqualTo(LocalDate.of(2023, 12, 11));
+        assertThat(date).isEqualTo(LocalDate.of(YEAR, MONTH, 1).plusDays(10));
+        assertThat(date).isEqualTo(LocalDate.of(YEAR, MONTH, 11));
     }
 
     @Test
     @DisplayName("[정상] 현재 데이트 정보와 입력된 데이터의 정보가 동일한지 테스트")
     void dateEqualsTest() {
-        assertThat(END_DATE.dateEquals(LocalDate.of(2023, 12, 31))).isTrue();
-        assertThat(END_DATE.dateEquals(LocalDate.of(2023, 12, 30))).isFalse();
-        assertThat(START_DATE.dateEquals(LocalDate.of(2023, 12, 1))).isTrue();
-        assertThat(START_DATE.dateEquals(LocalDate.of(2023, 12, 2))).isFalse();
+        assertThat(END_DATE.dateEquals(LocalDate.of(YEAR, MONTH, 31))).isTrue();
+        assertThat(END_DATE.dateEquals(LocalDate.of(YEAR, MONTH, 30))).isFalse();
+        assertThat(START_DATE.dateEquals(LocalDate.of(YEAR, MONTH, 1))).isTrue();
+        assertThat(START_DATE.dateEquals(LocalDate.of(YEAR, MONTH, 2))).isFalse();
     }
 
     @Test
     @DisplayName("[정상] 현재 기간과 입력된 기간 사이의 누적 일수")
     void getAccumulateDaysTest() {
-        assertThat(START_DATE.getAccumulateDays(LocalDate.of(2023, 12, 25))).isEqualTo(24);
-        assertThat(END_DATE.getAccumulateDays(LocalDate.of(2023, 12, 25))).isEqualTo(-6);
-        assertThat(CHRISTMAST.getAccumulateDays(LocalDate.of(2023, 12, 23))).isEqualTo(-2);
+        assertThat(START_DATE.getAccumulateDays(LocalDate.of(YEAR, MONTH, 25))).isEqualTo(24);
+        assertThat(END_DATE.getAccumulateDays(LocalDate.of(YEAR, MONTH, 25))).isEqualTo(-6);
+        assertThat(CHRISTMAST.getAccumulateDays(LocalDate.of(YEAR, MONTH, 23))).isEqualTo(-2);
     }
 
     @Test
     @DisplayName("[정상] 입력 된 일자가 이벤트 기간에 포함 되는지 테스트")
     void canEventPeriodTest() {
-        LocalDate otherDate = LocalDate.of(2023, 12, 23);
+        LocalDate otherDate = LocalDate.of(YEAR, MONTH, 23);
         assertThat(EventDate.canEventPeriod(otherDate)).isTrue();
-        LocalDate otherDate2 = LocalDate.of(2023, 11, 23);
+        LocalDate otherDate2 = LocalDate.of(YEAR, MONTH - 1, 23);
         assertThat(EventDate.canEventPeriod(otherDate2)).isFalse();
     }
 
     @Test
     @DisplayName("[정상] 입력 된 일자가 크리스마스 기간에 포함 되는지 테스트")
     void canChristmastPeriodTest() {
-        LocalDate otherDate = LocalDate.of(2023, 12, 25);
+        LocalDate otherDate = LocalDate.of(YEAR, MONTH, 25);
         assertThat(EventDate.canChristmastPeriod(otherDate)).isTrue();
-        LocalDate otherDate2 = LocalDate.of(2023, 11, 23);
+        LocalDate otherDate2 = LocalDate.of(YEAR, MONTH, 23);
         assertThat(EventDate.canChristmastPeriod(otherDate2)).isFalse();
     }
 }
