42a5db72910af5b3f87afe1072bcc0a034ad3f85
IMWoo94 <leemeawoo94@gmail.com>
2023-11-14 14:44:01 +0900

    refactor(Application): 메인 메소드 종료 시 Console close
    메인 메소드 실행 완료 후 Console 을 종료 시켜 준다.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,10 +1,17 @@
 package christmas;
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.RestaurantController;
 
 public class Application {
     public static void main(String[] args) {
-        RestaurantController restaurant = new RestaurantController();
-        restaurant.eventStart();
+        try {
+            RestaurantController restaurant = new RestaurantController();
+            restaurant.eventStart();
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            Console.close();
+        }
     }
 }
